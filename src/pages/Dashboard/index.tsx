752778526634ca933cdc47618f6e2bec4f6022bf
lucascprazeres <olucascostadp@gmail.com>
2020-06-29 14:44:35 -0300

    feat: add dashboard structure & style 🏠

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,63 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+import { Title, Form, Repositories } from './styles';
 
-import { Title } from  './styles';
+import logoImg from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
   return (
-    <Title>Explore Repositórios no Github</Title>
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore Repositórios no Github</Title>
+
+      <Form action="/">
+        <input placeholder="Digite o nome do repossitório" />
+        <button>
+          Pesquisar
+        </button>
+      </Form>
+
+      <Repositories>
+        <a href="/">
+          <img
+            src="https://avatars0.githubusercontent.com/u/51201126?s=400&u=62e4abe922d28596d36b0cd2479f257041af11eb&v=4"
+            alt="Lucas dos Prazeres" />
+
+          <div>
+            <strong>lucascprazeres/goBarber-server</strong>
+            <p>Primeiro projeto desenvolvido no bootcamp goStack 12</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="/">
+          <img
+            src="https://avatars0.githubusercontent.com/u/51201126?s=400&u=62e4abe922d28596d36b0cd2479f257041af11eb&v=4"
+            alt="Lucas dos Prazeres" />
+
+          <div>
+            <strong>lucascprazeres/goBarber-server</strong>
+            <p>Primeiro projeto desenvolvido no bootcamp goStack 12</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="/">
+          <img
+            src="https://avatars0.githubusercontent.com/u/51201126?s=400&u=62e4abe922d28596d36b0cd2479f257041af11eb&v=4"
+            alt="Lucas dos Prazeres" />
+
+          <div>
+            <strong>lucascprazeres/goBarber-server</strong>
+            <p>Primeiro projeto desenvolvido no bootcamp goStack 12</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
   );
 }
 
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,6 +1,99 @@
 import styled from 'styled-components';
+import { shade } from 'polished'
 
 export const Title = styled.h1`
   font-size: 48px;
   color: #3A3A3A;
+  max-width: 450px;
+  line-height: 56px;
+
+  margin-top: 80px;
+`;
+
+export const Form = styled.form`
+  margin-top: 40px;
+  max-width: 700px;
+
+  display: flex;
+
+  input {
+    flex: 1;
+    height: 70px;
+    padding: 0 24px;
+    border: 0;
+    border-radius: 5px 0 0 5px;
+    color: #3A3A3A;
+
+    &::placeholder {
+      color: #A8A8B3;
+    }
+  }
+
+  button {
+    width: 210px;
+    height: 70px;
+    background: #04d361;
+    border-radius: 0 5px 5px 0;
+    border: 0;
+    color: #FFF;
+    font-weight: bold;
+    transition: background-color 0.2s;
+
+    &:hover {
+      background: ${shade(0.2, '#04d361')};
+    }
+  }
+
+`;
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+
+  a {
+    background: #FFF;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    text-decoration: none;
+
+    display: flex;
+    align-items: center;
+
+    transition: transform 0.2s;
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    img {
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+    }
+
+    div {
+      margin-left: 16px;
+
+      strong {
+        font-size: 20px;
+        color: #3D3D4D;
+      }
+
+      p {
+        font-size: 18px;
+        color: #A8A8B3;
+        margin-top: 4px;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #CBCBD6;
+    }
+  }
 `;
